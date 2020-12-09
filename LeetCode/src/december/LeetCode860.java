5ebb094d975d1d0fac9b2395666416ad95237f36
lognteng <lognteng@qq.com>
2020-12-10 02:23:56 +0800

    add LeetCode860

---
+++ LeetCode/src/december/LeetCode860.java
@@ -0,0 +1,36 @@
+package december;
+
+public class LeetCode860 {
+    public static void main(String[] args) {
+        int[] a = {5,5,10,10,5,20,5,10,5,5};
+        System.out.println(Solution.lemonadeChange(a));
+    }
+}
+
+class Solution {
+    public static boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        for (int i = 0; i < bills.length; i++) {
+            if (bills[i] == 5) {
+                five ++;
+            }else if (bills[i] == 10) {
+                if (five <= 0) {
+                    return false;
+                }
+                five --;
+                ten ++;
+            }else if (bills[i] == 20) {
+                if (five >= 1 && ten >= 1) {
+                    five --;
+                    ten --;
+                }else if (five >= 3) {
+                    five -= 3;
+                }else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
